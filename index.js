a83e20a3df37ad9dab0351669d4767584951d8a1
An <anknguyenanknguyen@gmail.com>
2021-03-23 20:27:00 -0400

    :arrow_up: adjust hockey-rink to be based at 0,0 and transformed; upgrade to d3 v6

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 function index() {
-    d3.xml("resources/hockey-rink.svg", data => {
+    d3.xml("resources/hockey-rink.svg").then(data => {
         d3.select("#hockey-rink")
             .node()
             .append(data.documentElement);
+
+        d3.select("#hockey-rink")
+            .select("#outside-perimeter")
+            .on("click", e => {
+                console.log(d3.pointer(e));
+            });
     });
 }
 
