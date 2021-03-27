959fbc71845a8427c05e33da863d6d18f649d299
An <anknguyenanknguyen@gmail.com>
2021-03-27 13:34:31 -0400

    :heavy_plus_sign: add downloadjs

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import { setUpRink } from "./js/rink.js";
 import { setUpOptions } from "./js/options.js";
 import { setUpShots } from "./js/shots.js";
 import { setUpTable } from "./js/table.js";
+import { downloadCSV } from "./js/upload-download.js";
 function index() {
     d3.xml("resources/hockey-rink.svg").then(data => {
         setUpRink(data);
         setUpOptions();
         setUpTable();
         setUpShots();
+
+        d3.select("#download").on("click", downloadCSV);
     });
 }
 
---
+++ js/upload-download.js
@@ -0,0 +1,17 @@
+function downloadCSV() {
+    var csv = "Team,Player,X,Y\n";
+    d3.select("#shot-table-body")
+        .selectAll("tr")
+        .each(function() {
+            d3.select(this)
+                .selectAll("td")
+                .each(function() {
+                    csv += d3.select(this).text() + ",";
+                });
+            csv = csv.slice(0, -1) + "\n";
+        });
+    console.log(csv);
+    download(csv, "test.csv", "text/csv");
+}
+
+export { downloadCSV };
