c53865e43eefb31666a1f7096f40af9ec786e9c9
An <anknguyenanknguyen@gmail.com>
2021-10-27 23:39:48 -0700

    :art: used chained promises

--- setup.js
+++ setup.js
@@ -13,71 +13,73 @@ export let getDefaultDetails;
 
 function setup(s) {
     sport = s;
-    d3.json("/supported-sports.json").then(data => {
-        const sportData = _.find(data.sports, { id: sport });
-        cfgSportA = sportData.appearance;
-        getDefaultDetails = function() {
-            return _.cloneDeep(sportData.defaultDetails);
-        };
-    });
-    d3.xml(`/resources/${sport}.svg`).then(data => {
-        setUpPlayingArea(data);
-        setUpDetailsPanel();
-        setUpTable();
-        setUpShots();
-        setUpCSVDownloadUpload();
-        setUpLegend();
+    d3.json("/supported-sports.json")
+        .then(data => {
+            const sportData = _.find(data.sports, { id: sport });
+            cfgSportA = sportData.appearance;
+            getDefaultDetails = function() {
+                return _.cloneDeep(sportData.defaultDetails);
+            };
+            return d3.xml(`/resources/${sport}.svg`);
+        })
+        .then(data => {
+            setUpPlayingArea(data);
+            setUpDetailsPanel();
+            setUpTable();
+            setUpShots();
+            setUpCSVDownloadUpload();
+            setUpLegend();
 
-        d3.select("h1")
-            .attr("href", "./")
-            .on("click", () => {
-                window.location = "./";
+            d3.select("h1")
+                .attr("href", "./")
+                .on("click", () => {
+                    window.location = "./";
+                });
+
+            function decode(a) {
+                return a.replace(/[a-zA-Z]/g, function(c) {
+                    return String.fromCharCode(
+                        (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
+                            ? c
+                            : c - 26
+                    );
+                });
+            }
+
+            // https://www.ionos.com/digitalguide/e-mail/e-mail-security/protecting-your-email-address-how-to-do-it/
+
+            // ROT13 encryption for email
+            function decode(a) {
+                return a.replace(/[a-zA-Z]/g, function(c) {
+                    return String.fromCharCode(
+                        (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
+                            ? c
+                            : c - 26
+                    );
+                });
+            }
+
+            d3.select("#email").on("click", function() {
+                const y = "znvygb:naxathlranaxathlra@tznvy.pbz";
+                d3.select(this)
+                    .attr("href", decode(y))
+                    .on("click", () => {});
             });
 
-        function decode(a) {
-            return a.replace(/[a-zA-Z]/g, function(c) {
-                return String.fromCharCode(
-                    (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
-                        ? c
-                        : c - 26
-                );
+            $(document).ready(function() {
+                select2Dropdown();
+                $("#shot-type-select").on("change", function(e) {
+                    // update legend
+                    shotTypeLegend();
+
+                    // https://stackoverflow.com/a/54047075
+                    // do not delete new options
+                    $(this)
+                        .find("option")
+                        .removeAttr("data-select2-tag");
+                });
             });
-        }
-
-        // https://www.ionos.com/digitalguide/e-mail/e-mail-security/protecting-your-email-address-how-to-do-it/
-
-        // ROT13 encryption for email
-        function decode(a) {
-            return a.replace(/[a-zA-Z]/g, function(c) {
-                return String.fromCharCode(
-                    (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13)
-                        ? c
-                        : c - 26
-                );
-            });
-        }
-
-        d3.select("#email").on("click", function() {
-            const y = "znvygb:naxathlranaxathlra@tznvy.pbz";
-            d3.select(this)
-                .attr("href", decode(y))
-                .on("click", () => {});
         });
-
-        $(document).ready(function() {
-            select2Dropdown();
-            $("#shot-type-select").on("change", function(e) {
-                // update legend
-                shotTypeLegend();
-
-                // https://stackoverflow.com/a/54047075
-                // do not delete new options
-                $(this)
-                    .find("option")
-                    .removeAttr("data-select2-tag");
-            });
-        });
-    });
 }
 
 function setUpIndex() {
