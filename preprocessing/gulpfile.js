11b7ad98156c084aeb44fa2e36cb1ec1588b8b88
An <anknguyenanknguyen@gmail.com>
2023-02-11 09:50:44 -0800

    :chart_with_upwards_trend: re-enable analytics; add banners

--- preprocessing/gulpfile.js
+++ preprocessing/gulpfile.js
@@ -6,9 +6,9 @@ const inject = require("gulp-inject");
 const del = require("del");
 const sports = require("../supported-sports.json").sports;
 
-const indexBanner = false;
-const banner = false;
-const analytics = false;
+const indexBanner = true;
+const banner = true;
+const analytics = true;
 
 function html(sport) {
     return src("./base.html")
